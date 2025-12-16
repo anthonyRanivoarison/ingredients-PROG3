58d87e0fcbc9230e90a346ae25194459644de46d
Ryan Andriamahery <ryan@hei.school>
2025-12-16 14:53:52 +0300

    chore: add Dish and Ingredient classes and their components

---
+++ src/main/java/Ingredient.java
@@ -0,0 +1,86 @@
+import java.util.Objects;
+
+public class Ingredient {
+    private Integer id;
+    private String name;
+    private CategoryEnum category;
+    private Double price;
+    private Dish dish;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Integer id, String name, CategoryEnum category, Double price, Dish dish) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.dish = dish;
+    }
+
+    public String getDishName() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && category == that.category && Objects.equals(price, that.price) && Objects.equals(dish, that.dish);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, category, price, dish);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", price=" + price +
+                '}';
+    }
+}
